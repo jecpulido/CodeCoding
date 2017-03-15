082f0c60114fe020fdb8ed5ba3d0fcfd2e74cdb2
jecpulido <jorgepulido95@hotmail.com>
2017-03-15 17:31:22 -0500

    Cambios de footer

--- CodeCoding/footer.php
+++ CodeCoding/footer.php
@@ -4,29 +4,42 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="resource/css/bootstrap.min.css">
   <link rel="stylesheet" href="resource/css/footer.css">
+  <link rel="stylesheet" href="resource/css/bootstrap.min.css">
   <title>Document</title>
 </head>
 <body>
-  <div class="container">
-    <h1 class="text-center">
-      Siguenos en:
-    </h1>
+  <div class="container-fluid footer">
+
+
+
+
     <section class="main row">
-      <div class="col-xs-6 col-md-3">
+      <div class="visible-md visible-lg col-md-2">
+      </div>
+      <div class="col-xs-6 col-md-2">
         <a href="#" class="facebook" alt="Facebook" target="_blank">Facebook</a>
       </div>
-      <div class="col-xs-6 col-md-3">
-        <a href="#" class="twitter" alt=""></a>
+      <div class="col-xs-6 col-md-2">
+        <a href="#" class="twitter" alt="">Twitter</a>
       </div>
-      <div class="col-xs-6 col-md-3">
-        <a href="#" class="youtube" alt=""></a>
+      <div class="col-xs-6 col-md-2">
+        <a href="#" class="youtube" alt="">YouTube</a>
       </div>
-      <div class="col-xs-6 col-md-3">
-        <a href="#" class="google" alt=""></a>
+      <div class="col-xs-6 col-md-2">
+        <a href="#" class="google" alt="">Google</a>
+      </div>
+      <div class="col-xs-6 col-md-2">
       </div>
     </section>
+
+      <h2 style="font-size:50px">
+        &#60;/CodeCoding&#62;
+        <br>
+        <!-- {codeCoding} -->
+      </h2>
+
+
   </div>
   <script type="text/javascript" src="resource/js/jQuery.js"></script>
   <script type="text/javascript" src="resource/js/bootstrap.js"></script>
