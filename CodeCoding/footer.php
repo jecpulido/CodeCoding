3a744f73ea713b8435aa7ea3a5ad6fdd2186797e
jecpulido <jorgepulido95@hotmail.com>
2017-03-14 23:55:01 -0500

    Primer proyecto

---
+++ CodeCoding/footer.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="resource/css/bootstrap.min.css">
+  <link rel="stylesheet" href="resource/css/footer.css">
+  <title>Document</title>
+</head>
+<body>
+  <div class="container">
+    <h1 class="text-center">
+      Siguenos en:
+    </h1>
+    <section class="main row">
+      <div class="col-xs-6 col-md-3">
+        <a href="#" class="facebook" alt="Facebook" target="_blank">Facebook</a>
+      </div>
+      <div class="col-xs-6 col-md-3">
+        <a href="#" class="twitter" alt=""></a>
+      </div>
+      <div class="col-xs-6 col-md-3">
+        <a href="#" class="youtube" alt=""></a>
+      </div>
+      <div class="col-xs-6 col-md-3">
+        <a href="#" class="google" alt=""></a>
+      </div>
+    </section>
+  </div>
+  <script type="text/javascript" src="resource/js/jQuery.js"></script>
+  <script type="text/javascript" src="resource/js/bootstrap.js"></script>
+</body>
+</html>
---
+++ CodeCoding/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="/resource/css/bootstrap.min.css">
+  <title>Document</title>
+</head>
+<body>
+  <header>
+
+  </header>
+  <div class="container">
+    <section class="main">
+         <article class="col-md-10">
+           <h2>Titulo de Prueba</h2>
+           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+         </article>
+         <article class="col-md-2">
+           <h3>Titulo de Prueba</h3>
+           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+         </article>
+    </section>
+  </div>
+  <footer>
+    <div class="container">
+      <?php
+      include("footer.php") ?>
+    </div>
+  </footer>
+  <script type="text/javascript" src="/resource/js/jQuery.js"></script>
+  <script type="text/javascript" src="/resource/js/bootstrap.min.js"></script>
+</body>
+</html>
