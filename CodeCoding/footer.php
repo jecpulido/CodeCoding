7a3fbe79c05af426d6cc6b1ca452e4f42ccce046
jecpulido <jorgepulido95@hotmail.com>
2017-03-17 17:03:21 -0500

    Header menu

--- CodeCoding/footer.php
+++ CodeCoding/footer.php
@@ -4,36 +4,32 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="resource/css/footer.css">
-  <link rel="stylesheet" href="resource/css/bootstrap.min.css">
+
   <title>Document</title>
 </head>
 <body>
-  <div class="container-fluid footer">
+  <div class="container-fluid navbar-fixed-bottom footer">
     <section class="main row">
       <div class="visible-md visible-lg col-md-2">
       </div>
       <div class="col-xs-6 col-md-2">
-        <a href="#" class="facebook" alt="Facebook" target="_blank">Facebook</a>
+        <a href="#" class="facebook link" alt="Facebook" target="_blank">Facebook</a>
       </div>
       <div class="col-xs-6 col-md-2">
-        <a href="#" class="twitter" alt="">Twitter</a>
+        <a href="#" class="twitter link" alt="">Twitter</a>
       </div>
       <div class="col-xs-6 col-md-2">
-        <a href="#" class="youtube" alt="">YouTube</a>
+        <a href="#" class="youtube link" alt="">YouTube</a>
       </div>
       <div class="col-xs-6 col-md-2">
-        <a href="#" class="google" alt="">Google</a>
+        <a href="#" class="google link" alt="">Google</a>
       </div>
       <div class="col-xs-6 col-md-2">
       </div>
     </section>
-      <h2>
-        <!-- {codeCoding} -->
-        &#60;/CodeCoding&#62;
+      <h2>        <!-- {codeCoding} -->
+        
       </h2>
   </div>
-  <script type="text/javascript" src="resource/js/jQuery.js"></script>
-  <script type="text/javascript" src="resource/js/bootstrap.js"></script>
 </body>
 </html>
--- CodeCoding/index.php
+++ CodeCoding/index.php
@@ -4,15 +4,15 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, user-scalable=no,initial-scale=1.0, maxium-scale=1.0, minimum-scale=1.0 ">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="/resource/css/bootstrap.min.css">
+  <!-- <link rel="stylesheet" href="/resource/css/bootstrap.min.css"> -->
   <title>Document</title>
 </head>
 <body>
   <header>
-    <?php include("header.php") ?>
+    <?php include("header.php") ?>   
   </header>
-  <div class="container">
-    <section class="main">
+  <div class="container-fluid">
+    <section class="main row">
          <article class="col-md-10">
            <h2>Titulo de Prueba</h2>
            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
@@ -22,15 +22,27 @@
            <h3>Titulo de Prueba</h3>
            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
          </article>
+         <br>
+         <br>
+         <br>
+         <br>
+         <br>
+         <br>
+         <br>
+         <br>
+         <br>
+         <br>
+         <br>
+         <br>
+         <br>
+         <br>
     </section>
   </div>
-  <footer style="">
+  <footer class="">
     <div style="">
       <?php
       include("footer.php") ?>
     </div>
   </footer>
-  <script type="text/javascript" src="/resource/js/jQuery.js"></script>
-  <script type="text/javascript" src="/resource/js/bootstrap.min.js"></script>
 </body>
 </html>
