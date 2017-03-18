a2455aa3c52ef690292dbfc35e44f2a400397417
jecpulido <jorgepulido95@hotmail.com>
2017-03-18 14:12:16 -0500

    Pagina Principal

--- CodeCoding/header.php
+++ CodeCoding/header.php
@@ -5,66 +5,44 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="resource/css/estilos.css">
-    <link rel="stylesheet" href="resource/css/bootstrap.min.css">
+    <link rel="stylesheet" href="resource/css/bootstrap.css">
     <script type="text/javascript" src="resource/js/jquery-latest.js"></script>
     <script type="text/javascript" src="resource/js/bootstrap.js"></script>
     <title>Header</title>
   </head>
   <body class="menu">
-     <div class="container"> 
-       <header>
-         <nav class="navbar navbar-default navbar-fixed-top navbar-inverse" >
-           <div class="container-fluid headerColor">           
-             <div class="navbar-header">
-               <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar">
-                 <span class="sr-only">Menu</span>
-                 <span class="icon-bar"></span>
-                 <span class="icon-bar"></span>
-                 <span class="icon-bar"></span>
-               </button>
-               <a class="navbar-brand" href="#">{CodeCoding}</a>
-             </div>
-             <div class="collapse navbar-collapse" id="navbar">
-               <ul class="nav navbar-nav">
-                  <li><a href="https://www.w3schools.com/">Cursos</a></li>
-                  <!-- <li><a href="">item #2</a></li> -->
-                  <li><a href="">item #3</a></li>
-                  <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button">Dropdown <span class="caret"></span>
+    <nav class="navbar navbar-inverse navbar-fixed-top headerColor">
+      <div class="container ">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">{CodeCoding}</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+           <ul class="nav navbar-nav navbar-right">  
+               <li><a href="https://www.w3schools.com/">Programacion</a></li>
+                  <li><a href="https://www.w3schools.com/">Base de Datos</a></li>                  
+                  <li><a href="">Talleres</a></li>
+                  <li><a href="">Foro</a></li>   
+                  <li><a href="">Login</a></li>   
+                  <li class="hidden dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"> iniciar sesion <span class="glyphicon glyphicon-user "></span>
                     </a>
                     <ul class="dropdown-menu">
-                      <li><a href="a">item #1</a></li>
-                      <li><a href="a">item #2</a></li>
+                      <li><a href="a">Configuracion</a></li>
+                      <li><a href="a">Mis cursos</a></li>
                       <li class="divider"></li>
-                      <li><a href="a">item #4</a></li>
+                      <li><a href="a">Salir</a></li>
                     </ul>
                   </li>
                </ul>
-               <form action="" class="navbar-form navbar-left" role="search">
-                 <input type="text" name="" id="" class="form-control" placeholder="Buscar">
-                 <!-- <input type="submit" value="Buscar" class="form-control"> -->
-               </form>
-               <ul class="nav navbar-nav navbar-right">                 
-                  <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"> Usuario <span class="glyphicon glyphicon-user "></span>
-                    </a>
-                    <ul class="dropdown-menu">
-                      <li><a href="a">item #1</a></li>
-                      <li><a href="a">item #2</a></li>
-                      <li class="divider"></li>
-                      <li><a href="a">item #4</a></li>
-                    </ul>
-                  </li>
-               </ul>
-             </div>
-
-            
-
-           </div>
-         </nav>
-       </header>
-
-     </div>  
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
 
   </body>
 </html>
--- CodeCoding/index.php
+++ CodeCoding/index.php
@@ -10,32 +10,86 @@
 <body>
   <header>
     <?php include("header.php") ?>   
-  </header>
+  </header>   
+    <div class="jumbotron">
+      <div class="container">  
+        <div class="row">
+          
+          <div class="col-md-7">
+            <h1>{CodeCoding}</h1>
+            <p>Da el primer paso para aprender lo que te apasiona, aqui encontraras las bases para desarrollar las aplicaciones que desees en las plataformas que prefieras .</p>          
+          </div>
+          <div class="col-md-5">
+            <h2>Registrate</h2>
+            <form class="form-horizontal" action="" method="post">
+              <fieldset >
+                <div class="form-group ">
+                  <label for="usuario" class="sr-only hidden control-label col-md-2">Usuario</label>
+                  <div class="col-md-10">
+                    <input type="text" class="form-control" id="usuario" placeholder="Usuario">
+                  </div>
+                </div>
+
+                <div class="form-group ">
+                  <label for="correo" class="sr-only hidden control-label col-md-2">Correo</label>
+                  <div class="col-md-10">
+                    <input type="text" class="form-control" id="correo" placeholder="Correo">
+                  </div>
+                </div>
+
+                <div class="form-group ">
+                  <label for="pass" class="sr-only hidden control-label col-md-2">Contraseña</label>
+                  <div class="col-md-10">
+                    <input type="password" class="form-control" id="pass" placeholder="Contraseña">
+                  </div>
+                </div>
+
+                <div class="form-group ">
+                  <label for="Cpass" class="sr-only hidden control-label col-md-2">Confirmar contraseña</label>
+                  <div class="col-md-10">
+                    <input type="password" class="form-control" id="Cpass" placeholder="Confirmar contraseña">
+                  </div>
+                </div>
+
+                <div class="form-group">
+                <div class="col-md-2 ">
+                  <button type="button" class="btn btn-primary">Enviar</button>
+                </div>
+              </div>
+
+              </fieldset>
+            </form>
+          </div>
+        </div>      
+      </div>
+    </div>
+    
+
+
+    
   <div class="container-fluid">
-    <section class="main row">
-         <article class="col-md-10">
-           <h2>Titulo de Prueba</h2>
-           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-         </article>
-         <article class="col-md-2">
-           <h3>Titulo de Prueba</h3>
-           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-         </article>
-         <br>
-         <br>
-         <br>
-         <br>
-         <br>
-         <br>
-         <br>
-         <br>
-         <br>
-         <br>
-         <br>
-         <br>
-         <br>
-         <br>
+
+    <section class="main-row">
+      <div class="col-md-5">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
     </section>
   </div>
   <footer class="">
